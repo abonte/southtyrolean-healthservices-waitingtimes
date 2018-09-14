aeefde7ca8fdad65715a225d979a2800aa053928
bonte <andrea.bonte92@hotmail.it>
2018-09-14 14:12:57 +0200

    refactoring

--- app/views.py
+++ app/views.py
@@ -4,15 +4,18 @@ import requests
 import demjson
 from .forms import SearchForm
 import datetime
-from config import SUPPORTED_LANGUAGES, BABEL_DEFAULT_LOCALE    
+from config import SUPPORTED_LANGUAGES, BABEL_DEFAULT_LOCALE
 import json
+from operator import attrgetter
 
 bp = Blueprint('frontend', __name__, url_prefix='/<lang_code>')
 
+
 @babel.localeselector
 def get_locale():
-    #return request.accept_languages.best_match(SUPPORTED_LANGUAGES.keys())
+    # return request.accept_languages.best_match(SUPPORTED_LANGUAGES.keys())
     return g.get('lang_code', app.config['BABEL_DEFAULT_LOCALE'])
+
 
 @app.url_defaults
 def add_language_code(endpoint, values):
@@ -21,15 +24,18 @@ def add_language_code(endpoint, values):
     if app.url_map.is_endpoint_expecting(endpoint, 'lang_code'):
         values['lang_code'] = g.lang_code
 
+
 @app.url_value_preprocessor
 def pull_lang_code(endpoint, values):
     g.lang_code = values.pop('lang_code', None)
+
 
 @app.before_request
 def ensure_lang_support():
     lang_code = g.get('lang_code', None)
     if lang_code and lang_code not in app.config['SUPPORTED_LANGUAGES'].keys():
         return abort(404)
+
 
 @app.route('/', methods=('GET', 'POST'))
 @app.route('/index', methods=('GET', 'POST'))
@@ -40,62 +46,81 @@ def index():
     r = requests.get('http://daten.buergernetz.bz.it/services/WaitLists_Data/json')
     if r.status_code != 200:
         return render_template("index.html",
-                            title = 'Home',
-                            resultServices = "",
-                            services = "",
-                            check = False,
-                            form = form)
-    
-    #to use a local copy of the data
-    #with open('../waitListsDataExample.json') as data_file:    
+                               title='Home',
+                               resultServices="",
+                               services="",
+                               check=False,
+                               form=form)
+
+    # to use a local copy of the data
+    # with open('../waitListsDataExample.json') as data_file:
     #   data = json.load(data_file)
 
     data = demjson.decode(r.text)
 
     # aggiungere gestione errore
-    
-    resultServices=[]
-    if form.validate_on_submit():
-        # filter the services
-        for i in data:
-            if form.name.data.lower() in i['activityDescriptionIt'].lower() or form.name.data.lower() in i['activityDescriptionDe'].lower() :
-                resultServices.append(i)         
-    else: 
-        # all the services      
-        resultServices=data
 
-    services=set([])
+    resultServices = []
+    services = set([])
+    for i in data:
+            services = _addToListServices(services, i)
+            if form.validate_on_submit() and _isServiceDescription(form.name.data, i):
+                resultServices.append(i)
+
+    resultServices = data if not form.validate_on_submit() else resultServices
+
+    max_ = _findMaxWaiting(resultServices)
+
     for i in resultServices:
-        if g.lang_code == 'de':
-            services.add(i['activityDescriptionDe']) #list of services for the search form
-        else:
-            services.add(i['activityDescriptionIt'])
-        if i['waitingDays'] is not None:
-            i['waitingDaysPer']= round(i['waitingDays']*100/365, 2) #value for the progress bar
-        else:
-            #if waitingDay is not available
-            i['waitingDaysPer']= -1
-            i['waitingDays'] = 0
-        i['SurveyDate']= datetime.datetime.strptime(i['SurveyDate'], '%Y-%m-%dT%H:%M:%S').strftime('%d/%m/%y')
+        i['waitingDaysPer'] = _valueProgressBar(i, max_)
+        i['waitingDays'] = i['waitingDays'] if i['waitingDaysPer'] != -1 else 0
+        i['SurveyDate'] = datetime.datetime.strptime(i['SurveyDate'], '%Y-%m-%dT%H:%M:%S').strftime('%d/%m/%y')
 
     return render_template("index.html",
-                            title='Home',
-                            resultServices=resultServices,
-                            services=services,
-                            check = True,
-                            locale = g.lang_code,
-                            form = form)
+                           title='Home',
+                           resultServices=resultServices,
+                           services=services,
+                           check=True,
+                           locale=g.lang_code,
+                           form=form)
 
-''' @app.errorhandler(404)
+
+def _addToListServices(services, i):
+    if g.lang_code == 'de':
+        services.add(i['activityDescriptionDe'])  # list of services for the search form
+    else:
+        services.add(i['activityDescriptionIt'])
+    return services
+
+
+def _isServiceDescription(text, service):
+    return text.lower() in service['activityDescriptionIt'].lower() or text.lower() in service['activityDescriptionDe'].lower()
+
+
+def _valueProgressBar(elem, max_):
+    if elem['waitingDays'] is not None:
+        value = round(elem['waitingDays'] * 100 / max_, 2)  # value for the progress bar
+        return value if value > 10 else -1
+    else:
+        return -1
+
+
+def _findMaxWaiting(data):
+    return max([i['waitingDays'] for i in data], key=lambda i: i if i is not None else 0)
+
+
+@app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
 
 @app.errorhandler(500)
 def internal_server_error(error):
     app.logger.error('Server Error: %s', (error))
     return render_template('500.html'), 500
 
+
 @app.errorhandler(Exception)
 def unhandled_exception(error):
     app.logger.error('Unhandled Exception: %s', (error))
-    return render_template('500.html'), 500  '''
+    return render_template('500.html'), 500
